c7bd6c1365188d9886aa6d68a508335312be522f
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-11-28 17:13:44 -0300

    feat: add funcionalidade de troca de cor em background

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+
+const replaceImages = () => {
+    document.body.style.background = 'red'
+}
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault(); 
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id},
+    function: replaceImages,
+    });
+});
